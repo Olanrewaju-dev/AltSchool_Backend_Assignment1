09822166e9ab7ff47c8bf5f466605814fe634419
Balogun Olanrewaju <olanrewaju20@gmail.com>
2023-08-03 11:40:13 +0100

    modified node server using best practise

--- Q3.js
+++ Q3.js
@@ -1,11 +1,19 @@
-const http = require('http')
-const port = 3300
+const http = require('http');
+const port = 3300;
+const hostname = "localhost";
 
-const server = http.createServer((req, res)=> {
-    res.write("Hello World!")
-    res.end()
+const requestHandler = (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write("Hello World!");
+    res.end();
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, hostname, ()=> {
+    console.log(`Server running at http://${hostname}:${port}`);
 })
 
-server.listen(port, ()=> {
-    console.log(`Server running on port ${port}`)
-})
+
+
+
